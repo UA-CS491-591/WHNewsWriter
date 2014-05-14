d3048b98b87f417fbd73a7e1183ffa2d2c21710b
JackGerrity <jgerrity@cs.ua.edu>
2014-05-14 11:01:00 -0500

    Merge

--- WHWriterApp/WHWriterApp/ViewController.h
+++ WHWriterApp/WHWriterApp/ViewController.h
@@ -10,6 +10,4 @@
 
 @interface ViewController : UIViewController
 
--(IBAction)Next;
-
 @end
--- WHWriterApp/WHWriterApp/ViewController.m
+++ WHWriterApp/WHWriterApp/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController ()
 
+@property NSOperationQueue *operationQueue;
+
 @end
 
 @implementation ViewController
@@ -28,7 +30,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    _operationQueue = [[NSOperationQueue alloc]init];
     View *view = [[View alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [view.login addTarget:self action:@selector(didTapMyButton:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:view];
@@ -37,15 +39,33 @@
 
 -(void)didTapMyButton:(UIButton *)sender
 {
+    //Create url
+    NSURL *url = [NSURL URLWithString:@"https://mobileweb.caps.ua.edu/cs491/api/Account/login"];
     
-    [self Next];
+    //Create request object
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
+    //Let the server know that we want to interact in JSON
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    //Set http method
+    [request setHTTPMethod:@"POST"];
+    
+    //Specify the string to get sent to the server
+    NSString *loginString = @"{\"username\": \"zbarnes\",\"password\": \"password\"}";
+    //Make that string into raw data
+    NSData *loginData = [loginString dataUsingEncoding:NSUTF8StringEncoding];
+    //Set that raw data as the HTTP Body for the request
+    [request setHTTPBody:loginData];
+    
+    //Send asynchronous request
+    [NSURLConnection sendAsynchronousRequest:request queue:_operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        //Decode to string
+        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", responseString);
+        NavController *Navigation = [[NavController alloc] initWithNibName:nil bundle:nil];
+        [self presentViewController:Navigation animated:YES completion:NULL];
+    }];
 }
 
--(IBAction)Next
-{
-    NavController *Navigation = [[NavController alloc] initWithNibName:nil bundle:nil];
-    [self presentViewController:Navigation animated:YES completion:NULL];
-}
 
 - (void)didReceiveMemoryWarning
 {
