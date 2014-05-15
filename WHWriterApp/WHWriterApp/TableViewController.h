f5c1b1236022cf94795791857968aa5e341e4a7d
JackGerrity <jgerrity@cs.ua.edu>
2014-05-15 11:44:26 -0500

    Fixed search

--- WHWriterApp/WHWriterApp/TableViewController.h
+++ WHWriterApp/WHWriterApp/TableViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "LoginResponseObject.h"
 
-@interface TableViewController : UITableViewController
+@interface TableViewController : UITableViewController <UISearchBarDelegate>
 @property LoginResponseObject *user;
 -(void)didTapMyButton:(UIButton *)sender;
 @end
--- WHWriterApp/WHWriterApp/TableViewController.m
+++ WHWriterApp/WHWriterApp/TableViewController.m
@@ -53,17 +53,27 @@
     searchBar.placeholder = @"Search";
     self.tableView.tableHeaderView = searchBar;
     
-    UISearchDisplayController *searchDC = [[UISearchDisplayController alloc] initWithSearchBar:searchBar contentsController:self];
+    //UISearchDisplayController *searchDC = [[UISearchDisplayController alloc] initWithSearchBar:searchBar contentsController:self];
     
     // The above assigns self.searchDisplayController, but without retaining.
     // Force the read-only property to be set and retained.
-    [self performSelector:@selector(setSearchDisplayController:) withObject:searchDC];
-    
+    //[self performSelector:@selector(setSearchDisplayController:) withObject:searchDC];
+    /*
     searchDC.delegate = self;
     searchDC.searchResultsDataSource = self;
     searchDC.searchResultsDelegate = self;
+    */
     
 }
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    if ([searchText  isEqual: @""]) {
+        [self refreshTable];
+    }
+    else {
+        [self refreshTableOnSearch:searchText];
+    }
+}
+
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (tableView == self.searchDisplayController.searchResultsTableView) {
         ;//self.tableView.tableViewData = searchResultsData objectatindex...; //array with filtered data
@@ -88,10 +98,10 @@
     [self.navigationController pushViewController:vc2 animated:YES];
 }
 
--(void)refreshTable{
+-(void)refreshTableOnSearch:(NSString *)text{
     
     //Create url
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@%@", @"https://mobileweb.caps.ua.edu/cs491/api//Story/byAuthor?token=", self.user.accessToken, @"&authorId=",self.user.user.Id]];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@%@%@%@", @"https://mobileweb.caps.ua.edu/cs491/api/Story/search?token=", self.user.accessToken, @"&searchString=",text,@"&authorId=",self.user.user.Id]];
     
     //Create request object
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
@@ -108,15 +118,30 @@
             [self.tableView reloadData];
         });
     }];
+}
+
+-(void)refreshTable{
     
+    //Create url
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@%@", @"https://mobileweb.caps.ua.edu/cs491/api/Story/byAuthor?token=", self.user.accessToken, @"&authorId=",self.user.user.Id]];
+    
+    //Create request object
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:5.0];
+    //Let the server know that we want to interact in JSON
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    //Set http method
+    
+    //Send asynchronous request
+    [NSURLConnection sendAsynchronousRequest:request queue:_operationQueue completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        //Decode to string
+        _stories = [NSObject arrayOfType:[StoryObject class] FromJSONData:data];
+        //Hop back main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
 }
 
-
--(IBAction)Next
-{
-    //NavController *Navigation = [[NavController alloc] initWithNibName:nil bundle:nil];
-    //[self presentViewController:Navigation animated:YES completion:NULL];
-}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -124,13 +149,6 @@
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return _stories.count;
-}
 
 /*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
