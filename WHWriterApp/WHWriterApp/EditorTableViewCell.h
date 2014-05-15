4bac5d487d2e7052fe80051ef7c85bf8e269fda4
Jonathan Fisher <jonathanzfisher@gmail.com>
2014-05-15 10:50:13 -0500

    Working with EditorTableViewCell

---
+++ WHWriterApp/WHWriterApp/EditorTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EditorTableViewCell.h
+//  WHWriterApp
+//
+//  Created by Jonathan Fisher on 5/15/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditorTableViewCell : UITableViewCell
+
+@end
---
+++ WHWriterApp/WHWriterApp/EditorTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  EditorTableViewCell.m
+//  WHWriterApp
+//
+//  Created by Jonathan Fisher on 5/15/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import "EditorTableViewCell.h"
+
+@implementation EditorTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
