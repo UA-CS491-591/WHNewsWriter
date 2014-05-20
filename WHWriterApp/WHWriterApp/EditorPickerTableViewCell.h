4e3a505dc0dc261dc71d47295ae67462e232b351
JackGerrity <jgerrity@cs.ua.edu>
2014-05-20 09:55:43 -0500

    Added a picker and fixed some linking issue

---
+++ WHWriterApp/WHWriterApp/EditorPickerTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  EditorPickerTableViewCell.h
+//  WHWriterApp
+//
+//  Created by Jack Gerrity on 5/20/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditorPickerTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
+
+@end
