78cb44618920c5721c2313ac8fd36650a811db17
Jonathan Fisher <jonathanzfisher@gmail.com>
2014-05-22 00:04:18 -0500

    Major Changes to the Editor. Completely redone, w/ body's label still needing work with.

---
+++ WHWriterApp/WHWriterApp/TextViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextViewController.h
+//  WHWriterApp
+//
+//  Created by Jonathan Fisher on 5/21/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewController : UIViewController
+
+@end
---
+++ WHWriterApp/WHWriterApp/TextViewController.m
@@ -0,0 +1,38 @@
+//
+//  TextViewController.m
+//  WHWriterApp
+//
+//  Created by Jonathan Fisher on 5/21/14.
+//  Copyright (c) 2014 CS491. All rights reserved.
+//
+
+#import "TextViewController.h"
+
+@interface TextViewController ()
+
+@end
+
+@implementation TextViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
